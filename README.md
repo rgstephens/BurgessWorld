67b94e64f5c360ec46645c2f2409a0bcfe04e498
Greg Stephens <greg@udon.org>
2017-06-11 15:23:56 -0700

    v0.6 - MQTT Support

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# BurgessWorld
-A repository for documents related to BurgessWorld Custom Electronics Projects
+This is a fork of the BurgessWorld [example sketch](https://github.com/cburgess5294/BurgessWorld) implementing MQTT instead of ThingSpeak.com to report DHT temp/humidity.
+
+## BurgessWorld Battery Powered Temperature Board
+
+- [BurgessWorld Battery Temperature board](https://www.tindie.com/products/cburgess129/battery-powered-wifi-temphumidity-sensor/).  [Github repository](https://github.com/cburgess5294/BurgessWorld)
+
+## Arduino Libraries Used
+
+- WiFiEsp.h, [bportaluri](https://github.com/bportaluri/WiFiEsp) library [know](https://github.com/bportaluri/WiFiEsp/issues/37) to work with PubSubClient
+- PubSubClient.h, [Arduino Client for MQTT](http://pubsubclient.knolleary.net/)
+
+## Change Log
+
+### v0.6
+
+- Replace WiFi library with [bportaluri WiFiEsp.h](https://github.com/bportaluri/WiFiEsp)
+- Use MQTT instead of ThingSpeak to report DHT temp/humidity
+- Report Arduino IP address on boot via MQTT
+
+## MQTT & Arduino
+
+This projects is inspired by the MickMake [Letterbox project](https://mickmake.com/archives/2648) with the github repository [here](https://github.com/MickMakes/MQTTletterbox) and the BurgessWorld [Battery-Powered WiFi Temp/Humditity Sensor board](https://www.tindie.com/products/cburgess129/battery-powered-wifi-temphumidity-sensor/).
+
+MQTT example code was taken from the [bportaluri/WiFiEsp](https://github.com/bportaluri/WiFiEsp/issues/37) project.
+
+To use the [Arduino ESP8266 library](https://github.com/esp8266/Arduino) in the Arduino Preferences add the following to the list of Additional Boards Manager URLs:
+
+- Enter http://arduino.esp8266.com/stable/package_esp8266com_index.json into Additional Board Manager URLs field. You can add multiple URLs, separating them with commas.
+
+- Open Boards Manager from Tools > Board menu and install esp8266 platform (and don't forget to select your ESP8266 board from Tools > Board menu after installation).
+
+Added [Arduino Client for MQTT](http://pubsubclient.knolleary.net/).
+
+## ESP-01 Firmware information
+
+```
+AT version:1.2.0.0(Jul  1 2016 20:04:45)
+SDK version:1.5.4.1(39cb9a32)
+Ai-Thinker Technology Co. Ltd.
+Dec  2 2016 14:21:16
+```
+
+# Misc Notes Taken During The Poroject
+
+## ESP8266 Boards
+
+Review of [ESP8266 boards](https://openhomeautomation.net/choose-esp8266-module/)
+
+## ESP-01 Firmware Options
+
+- [Espressif](http://bbs.espressif.com/viewforum.php?f=46)
+- [NodeMCU](https://nodemcu.readthedocs.io/en/master/en/flash/)
+
+ESP-01 [serial port connection](http://fab.cba.mit.edu/classes/863.14/tutorials/Programming/serialwifi.html)
+
+Debut ESP-01 from [Arduino code](http://yaab-arduino.blogspot.com/2015/03/debugging-esp8266-commands-with-arduino.html)
+
+Good ESP-01 [introduction and tutorial](http://www.instructables.com/id/Getting-Started-With-the-ESP8266-ESP-01/)
+
+Description of [ESP-01 tools](https://gist.github.com/stonehippo/3d9eb100d4f545015515)
+
+## ESP8266 Libraries
+
+- Standard WiFi Shield [Library](https://www.arduino.cc/en/Reference/WiFi)
+- ESP8266.h, [Itead](https://github.com/itead/ITEADLIB_Arduino_WeeESP8266) used by Burgessworld
+- ESP8266WiFi.h, [esp8266](https://github.com/esp8266/Arduino) used by MickMake Letterbox
+- Adafruit_ESP8266.h, Adafruit ESP8266
+- WiFiEsp.h, [bportaluri](https://github.com/bportaluri/WiFiEsp) library [know](https://github.com/bportaluri/WiFiEsp/issues/37) to work with PubSubClient
+
+## MQTT Libraries
+
+- PubSubClient.h, [Arduino Client for MQTT](http://pubsubclient.knolleary.net/) used by MickMake Letterbox
+- Adafruit_MQTT.h, [Arduino MQTT Library](https://github.com/adafruit/Adafruit_MQTT_Library)
+- MQTTClient.h, [256dpi Library](https://github.com/256dpi/arduino-mqtt) Paho based(limited ESP8266 boards)
--- ThingSpeak_temperature_humidity_example.ino
+++ ThingSpeak_temperature_humidity_example.ino
@@ -1,40 +1,48 @@
-/*  This example sketch is designed for use with Basic and Advanced Sensor Boards sold by BurgessWorld Custom Electronics
- *  The sketch uses an ESP8266 wifi module and a DHT temp/humidity sensor to upload temp/humidity data to a 
- *  thingspeak.com channel every 15 minutes.  
- *  It assumes a GREEN LED is attached to pin 5, and a RED LED is attached to pin 13.
+/*  This is a fork of the BurgessWorld example sketch implementing MQTT instead of ThingSpeak.com to report
+ *  DHT temp/humidity
+ *
  *  The LEDs can be disabled by commenting out "#define EnableLEDs" in the sketch below
  *  It is based on the ESP8266 wifi library available here:  https://github.com/itead/ITEADLIB_Arduino_WeeESP8266
  *  The DHT library available here:  https://github.com/adafruit/DHT-sensor-library/archive/master.zip
  *  And the Adadruit sensor library available here:  https://github.com/adafruit/Adafruit_Sensor/archive/master.zip
- *  The sketch takes a sensor reading every 15 minutes and uploads it to the defined thingspeek channel
  *  After uploading the data, the ESP8266 module is put to sleep until it is time to read the sensor again
- *  You should define your local WiFi settings, and your thingspeak channel write API key.
+ *  You should define your local WiFi settings, and your MQTT parameters.
  *  You can add additional sensors and upload additional data by including the appropriate library
  *  and tweeking the "Build Data Package" section in the sendMessage function
  *  This sketch is provided AS-IS with no implied warranty for fitness of purpose
  *  You may modify this sketch to meet your needs
+ *
+ *  Arduino C++ Reference: https://www.arduino.cc/en/reference/homePage
  */
+#include <doxygen.h>
+#include "WiFiEsp.h"
+#include <PubSubClient.h>
 #include "SoftwareSerial.h"
 #include "ESP8266.h"
 #include <DHT.h>
 
+#define PROG_NAME "BurgessWorldMQTT, "
+#define PROG_VERS "v0.6"
+
 //comment out the following line to disable status LEDS when running on batteries
 #define EnableLEDs
+
+// Sleep time - 1m = 60000, 5m = 300000, 15m = 900000
+#define DelayMS 60000
+unsigned long confTempDelay = DelayMS;    // Default temperature publish delay.
 
 //Define status indicator LED pins
 #define GREEN 5
 #define RED 13
 
 //define wifi SSID and password
-#define SSID        "YOUR SSID"
-#define PASSWORD    "YOUR PASSWORD"
+#define WIFI_SSID        "nworks24"  // nworks24, VillaLibertate
+#define WIFI_PASSWORD    "golfbgc1"  // golfbgc1, clanWa11ac3
 
-//define thingspeak address and port
-#define HOST_NAME   "api.thingspeak.com"
-#define HOST_PORT   (80)
-
-//define thingspeek channel write key
-#define channelKey "YOUR CHANNEL WRITE API KEY"
+//MQTT params
+#define MQTT_SERVER  "15.1.4.4"
+#define MQTT_PORT    8883
+#define MQTT_TOPICBASE "arduino/"
 
 // Define DHT Sensor type - uncomment whatever type you're using
 //#define DHTTYPE DHT11   // DHT 11
@@ -56,6 +64,10 @@ struct reading {
 //define Software Serial instance
 SoftwareSerial Serial1(7, 6);  // RX, TX
 
+WiFiEspClient espClient;
+//WiFiClient client;
+PubSubClient mqttClient(espClient);
+
 //create ESP8266 instance
 ESP8266 wifi(Serial1, 57600);
 
@@ -67,14 +79,136 @@ bool connectStat;
 int failCount;
 bool espPresent = false;
 
+void printFullWifiStatus() {
+  Serial.print("WiFi Status: ");
+  switch (WiFi.status()) {
+    case WL_CONNECTED:
+      Serial.print("WL_CONNECTED");
+      break;
+    case WL_NO_SHIELD:
+      Serial.print("WL_NO_SHIELD");
+      break;
+    case WL_IDLE_STATUS:
+      Serial.print("WL_IDLE_STATUS");
+      break;
+/*
+    case WL_NO_SSID_AVAIL:
+      Serial.println("WL_NO_SSID_AVAIL");
+      break;
+    case WL_SCAN_COMPLETED:
+      Serial.println("WL_SCAN_COMPLETED");
+      break;
+*/
+    case WL_CONNECT_FAILED:
+      Serial.print("WL_CONNECT_FAILED");
+      break;
+/*
+    case WL_CONNECTION_LOST:
+      Serial.println("WL_CONNECTION_LOST");
+      break;
+*/
+    case WL_DISCONNECTED:
+      Serial.print("WL_DISCONNECTED");
+      break;
+    default:
+      Serial.print("**Unknown response: ");
+      Serial.print(WiFi.status());
+    break;
+  }
+
+  // print the SSID of the network you're attached to
+  Serial.print(", SSID: ");
+  Serial.print(WiFi.SSID());
+
+  // print your WiFi shield's IP address
+  IPAddress ip = WiFi.localIP();
+  Serial.print(", IP: ");
+  Serial.print(ip);
+
+  // print the received signal strength
+  long rssi = WiFi.RSSI();
+  Serial.print(", Signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+String IpAddress2String(const IPAddress& ipAddress)
+{
+  return String(ipAddress[0]) + String(".") +\
+  String(ipAddress[1]) + String(".") +\
+  String(ipAddress[2]) + String(".") +\
+  String(ipAddress[3])  ;
+}
+
+void setupWifi() {
+  Serial.println(F(">> setupWifi <<"));
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+//  while (WiFi.status() != WL_CONNECTED) {
+//    delay(2000);
+//    Serial.print(".");
+//  }
+
+  printFullWifiStatus();
+
+  Serial.print("Set MQTT server: ");
+  Serial.println(MQTT_SERVER);
+  mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
+  mqttClient.setCallback(mqttCallback);
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int length)
+{
+  payload[length] = 0;    // Hack to be able to use this as a char string.
+
+  if (strstr(topic, MQTT_TOPICBASE "config/"))
+  {
+    if (strstr(topic, "TempDelay"))
+          confTempDelay = atoi((const char *)payload);
+  }
+}
+
+void mqttReconnect()
+{
+  // Loop until we're reconnected
+  while (!mqttClient.connected())
+  {
+    printFullWifiStatus();
+    // Attempt to connect
+    //if (mqttClient.connect("LetterBox", "letterbox", "letterbox", (char *)MQTT_TOPICBASE "State", 1, 0, "DEAD"))
+    if (mqttClient.connect((char *)MQTT_TOPICBASE "state"))
+    //if (mqttClient.connect("ESP8266Client"))
+    {
+      Serial.println(F("connected to mqtt server"));
+      // Once connected, publish an announcement...
+      Publish((char *)"state", (char *)"boot");
+      //IPAddress ip = WiFi.localIP();
+      String strIp = IpAddress2String(WiFi.localIP());
+      Publish((char *)"ip", strIp.c_str());
+      // Subscribe to enable bi-directional comms.
+      mqttClient.subscribe(MQTT_TOPICBASE "config/#");  // Allow bootup config fetching using MQTT persist flag!
+      mqttClient.subscribe(MQTT_TOPICBASE "put/#");     // Send commands to this device, use Home/LetterBox/Get/# for responses.
+    }
+    else
+      delay(5000);
+  }
+}
+
 void setup() {
   Serial.begin(57600);
-  Serial.println(F("Started"));
+  Serial.print(F("Started, "));
+  Serial.print(PROG_NAME);
+  Serial.println(PROG_VERS);
 
   //set pin mode for status indicator LED pins
   pinMode(GREEN, OUTPUT);
   pinMode(RED, OUTPUT);
-   
+
   //set pin mode for DHT22 output pin
   pinMode(DHTPIN, INPUT_PULLUP);
 
@@ -88,34 +222,52 @@ void setup() {
 
   //wait for ESP status
   if (recFind("ready", 5000)){
-    Serial.println(F("Ready"));
-    //set ESP8266 to station mode
-    if (wifi.setOprToStation()){
-      Serial.println(F("Station mode enabled"));
-      Serial.println(F("ESP initialized"));
-      flushSerial();
-      flashLED(GREEN, 1);
-      initEsp();
-      espPresent = true;
+    Serial.println(F("ESP Ready"));
+
+    WiFi.init(&Serial1);
+
+    printFullWifiStatus();
+    // check for the presence of the shield
+    if (WiFi.status() == WL_NO_SHIELD) {
+      Serial.println("WiFi shield not present");
+      // don't continue
+      while (true);
     }
+
+    flashLED(GREEN, 1);
+
+    // connect to WiFi network
+    setupWifi();
+    espPresent = true;
   }
   else{
     Serial.println(F("ESP not found"));
     Serial.println(F("...check ESP UART Speed"));
     Serial.println(F("...verify ESP LED activity"));
     flashLED(RED, 5);
+    delay(3000); //delay 15 minutes
+    Serial.println(F("...verify ESP LED activity"));
+    flashLED(RED, 5);
+    delay(3000); //delay 15 minutes
+    flashLED(RED, 5);
+    delay(3000); //delay 15 minutes
   }
 
 }
 
 void loop() {
   if (espPresent){
+    if (!mqttClient.connected()) {
+      mqttReconnect();
+    }
     //read sensor
     reading_r = readSensor();
+    printFullWifiStatus();
     //send message
-    sendMessage();
+    PublishInt("temp", reading_r.f);
+    PublishInt("humidity", reading_r.h);
     //put ESP to sleep
-    Serial.println(F("Put ESP to sleep"));
+    Serial.print(F("Put ESP to sleep... "));
     bool espAsleep = false;
     while (!espAsleep){
       Serial1.println(F("AT+SLEEP=2")); //put the ESP8266 to sleep
@@ -128,9 +280,11 @@ void loop() {
         flushSerial();
       }
     }
-    delay(900000); //delay 15 minutes
+    delay(DelayMS); //delay 15 minutes
+    Serial.print(PROG_NAME);
+    Serial.println(PROG_VERS);
     Serial.println(F("Initialize ESP"));
-    initEsp();
+    setupWifi();
   }
 }
 
@@ -177,77 +331,6 @@ bool recFind(String target, uint32_t timeout)
    return false;
 }
 
-//function to reset ESP
-void initEsp(){
-  bool espReady = false;
-  bool resetStatus = false;
-  while (!resetStatus){
-    //reset ESP
-    digitalWrite(ESPRESETPIN, LOW);
-    delay(300);
-    digitalWrite(ESPRESETPIN, HIGH);
-    //wait for esp ready
-    if (recFind("ready", 5000)){
-      Serial.println(F("ESP Ready"));
-      espReady = true;
-      flashLED(GREEN, 2);
-      }
-    else {
-      Serial.println(F("ESP not ready"));
-      flashLED(RED, 2);
-      }
-    //check wifi connection
-    if (espReady){
-      //wait for wifi connection
-      bool wifiJoined = false;
-      if (recFind("GOT IP", 5000)){
-        wifiJoined = true;
-        } 
-      if (wifiJoined){
-        Serial.println(F("Wifi connected"));
-        flashLED(GREEN, 3);
-        resetStatus = true;
-        Serial.print("IP:");
-        Serial.println(wifi.getLocalIP().c_str());
-        }
-      //join wifi if not connected
-      else {
-        Serial.println(F("wifi not joined")); 
-        //check to see if esp is responding
-        if (wifi.kick()){
-        Serial.println(F("ESP is up"));
-        Serial1.println(F("AT+CWJAP?"));
-          if (recFind("No AP", 5000)){
-            Serial.println(F("No AP configured"));
-            flushSerial();
-            //join wifi
-            if (wifi.joinAP(SSID, PASSWORD)){
-              Serial.println(F("wifi joined"));
-              flashLED(GREEN, 3);
-              resetStatus = true;
-              Serial.print("IP:");
-              Serial.println( wifi.getLocalIP().c_str());
-            }
-            else {
-              Serial.println(F("failed to join wifi"));
-              flashLED(RED, 3);
-              }
-          }
-          else {
-            Serial.println(F("no response to AT+CWJAP command"));
-            flashLED(RED, 3);
-            }
-        }
-        else {
-          Serial.println(F("ESP didn't respond to kick"));
-          flashLED(RED, 3);
-          }
-        }
-      }
-    flushSerial();
- }
-}
-
 struct reading readSensor(){
   uint8_t sumReadHUM = 0;
   uint8_t sumReadTMP = 0;
@@ -257,6 +340,7 @@ struct reading readSensor(){
   pinMode(DHTPIN, INPUT);
   delay(100);
   Serial.println(F("read sensor"));
+  //Serial.println(DateTime.now());
   //read sensor and discard first reading
   trashReadTMP += dht.readTemperature(true);
   trashReadHUM += dht.readHumidity();
@@ -271,81 +355,48 @@ struct reading readSensor(){
   //add readings to struct and return
   reading_r.h = (sumReadHUM / 2);
   reading_r.f = (sumReadTMP / 2);
+  Serial.print(F("Humidity: "));
   Serial.println(reading_r.h);
+  Serial.print(F("Fahreheit: "));
   Serial.println(reading_r.f);
   return reading_r;
 }
 
-void sendMessage(){
-  connectStat = 0;
-  int disableCount;
-  while (!connectStat){
+void Publish(char *Topic, char *Message)
+{
+  char TopicBase[80] = MQTT_TOPICBASE;
 
-    //open tcp conncection to thingspeek.com
-    if (wifi.createTCP(HOST_NAME, HOST_PORT)){
-      Serial.println(F("TCP connected"));
-      flashLED(GREEN, 4);
-    
-      //build data package
-      String outmessage = "GET /update?key=";
-      outmessage+=channelKey;
-      outmessage+="&field1=";
-      outmessage+=reading_r.h;
-      /* you can create addditional field definitions to upload more data fields by duplicating the following 
-      *  two lines of code and incrementing the 'field#' number in the first line and specifying another variable 
-      *  to upload in the second line */
-      outmessage+="&field2=";
-      outmessage+=reading_r.f;
-      //add the rest of the data packet
-      outmessage+=" HTTP/1.0\r\n\r\n";
-
-      //find length of outmessage and add 1
-      int outmessage_len = outmessage.length() + 1; 
-      // Prepare the character array (the buffer) 
-      char outChar[outmessage_len];
-      // Copy string message to character array for sending
-      outmessage.toCharArray(outChar, outmessage_len);
-  
-      //Send data package
-      if (wifi.send((const uint8_t*)outChar, outmessage_len)){
-        Serial.println(F("send success!"));
-        flashLED(GREEN, 5);
-        flushSerial();
-        connectStat = 1;
-      }
-      else {
-        Serial.println(F("send failed"));
-        Serial1.println(F("AT+CIPCLOSE"));
-        flashLED(RED, 5);
-     }
-   }
-   else {
-      Serial.println(F("socket error"));
-      Serial1.println(F("AT+CIPCLOSE"));
-      flashLED(RED, 4);
-  }
-  if (connectStat){
-    failCount = 0;
-    disableCount = 0;
-  }
-  else {
-    if (disableCount == 3){
-      failCount = 0;
-      disableCount = 0;
-      connectStat = 1;
-    }
-    else {
-      if (failCount == 3){
-        flushSerial();
-        initEsp();
-        failCount = 0;
-        disableCount++;
-      }
-      else {
-        failCount++;
-       }
-     }
-    }
-   flushSerial();
-  }
+  strcat(TopicBase, Topic);
+  mqttClient.publish(TopicBase, Message);
 }
+
+
+void PublishInt(char *Topic, int Value)
+{
+  char TopicBase[80] = MQTT_TOPICBASE;
+  char Message[10] = "NULL";
+
+  if (!isnan(Value))
+    itoa(Value, Message, 10);
+
+  strcat(TopicBase, Topic);
+  Serial.print("PublishInt ");
+  Serial.print(TopicBase);
+  Serial.print(" = ");
+  Serial.println(Message);
+  mqttClient.publish(TopicBase, Message);
+}
+
+
+void PublishFloat(char *Topic, float Value)
+{
+  char TopicBase[80] = MQTT_TOPICBASE;
+  char Message[10] = "NULL";
+
+  if (!isnan(Value))
+    dtostrf(Value, 5, 2, Message);
+
+  strcat(TopicBase, Topic);
+  mqttClient.publish(TopicBase, Message);
+}
+
